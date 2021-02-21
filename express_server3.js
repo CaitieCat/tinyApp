ab6ea4fc55e697da8b0d7971ffc694d744fe9ad8
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-21 18:35:20 -0500

    Adding a function to generate an ID and setting up POST requests

--- express_server3.js
+++ express_server3.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+const randomID = function generateRandomString() {
+  let randomString = Math.random().toString(36).slice(2, 8);
+  return randomString;
+};
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,9 +20,18 @@ const urlDatabase = {
 
 //render the /urls route to show all urls
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index3", templateVars);
-  });
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index3", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new3");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.param.longURL};
